d49e3d73fa2275a755b1771c9ad991e5b216e13c
Pedro Araújo <pedrinho.hipolito@gmail.com>
2026-09-08 23:13:07 -0300

    Repetições

---
+++ repeticoes/ambiente.js
@@ -0,0 +1,16 @@
+c = 0
+c2 = 0
+
+while (c < 5) {
+    c ++
+    console.log(`Repetição ${c}`)
+}
+
+do {
+    c2 ++
+    console.log(`Repeticion ${c2}`)
+} while (c2 < 5)
+
+for (var c3 = 1; c3 <= 5; c3 ++) {
+    console.log(`Repetition ${c3}`)
+}
